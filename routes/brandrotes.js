19fc14ecbc6884cb9d3c4d63033fdf51927194da
emmycode53 <dope98577@gmail.com>
2025-08-17 00:44:09 +0100

    fixed routes

--- routes/brandrotes.js
+++ routes/brandrotes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const brand = require('../controller/brandcontroller');
 const route = express.Router();
 
-route.use('/', brand.addBrand);
-route.use('/', brand.updateBrand);
-route.use('/', brand.getBrands);
-route.use('/', brand.deleteBrand);
+route.post('/', brand.addBrand);
+route.put('/', brand.updateBrand);
+route.get('/', brand.getBrands);
+route.delete('/', brand.deleteBrand);
 
 module.exports = route;
 
